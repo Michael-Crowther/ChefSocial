736c4d40a22a503c3c4b73fe62755670fe6819f6
Michael-Crowther <md.crowther@yahoo.com>
2025-01-14 08:24:28 -0700

    Initial Commit

---
+++ ChefSocial/ChefSocialApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChefSocialApp.swift
+//  ChefSocial
+//
+//  Created by Michael Crowther on 1/14/25.
+//
+
+import SwiftUI
+
+@main
+struct ChefSocialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChefSocialTests/ChefSocialTests.swift
@@ -0,0 +1,17 @@
+//
+//  ChefSocialTests.swift
+//  ChefSocialTests
+//
+//  Created by Michael Crowther on 1/14/25.
+//
+
+import Testing
+@testable import ChefSocial
+
+struct ChefSocialTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
